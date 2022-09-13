9b38caf265bae7efff8196dc1ca6d636e937e49f
YaroslavAntoniuk <yaroslav.antoniuk@binary-studio.com>
2022-09-13 17:06:39 +0300

    Implemented Error handling ticket

--- src/index.js
+++ src/index.js
@@ -5,17 +5,9 @@ import UsersDAO from "./dao/usersDAO"
 import CommentsDAO from "./dao/commentsDAO"
 
 const port = process.env.PORT || 8000
-/**
-Ticket: Timeouts
-
-Please prevent the program from waiting indefinitely by setting the write
-concern timeout limit to 2500 milliseconds.
-*/
 
 MongoClient.connect(
   process.env.MFLIX_DB_URI,
-  // TODO: Timeouts
-  // Set the write timeout limit to 2500 milliseconds.
   { useNewUrlParser: true, poolSize: 50, wtimeout: 2500 },
 )
   .catch(err => {
